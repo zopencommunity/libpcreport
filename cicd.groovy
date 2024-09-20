9c1bf80e3021094ef21b90d237491ff473e11da9
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:22:06 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux')
 {
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libpcreport.git'), string(name: 'PORT_DESCRIPTION', value: 'Perl Compatible Regular Expressions' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libpcreport.git'), string(name: 'PORT_DESCRIPTION', value: 'Perl Compatible Regular Expressions' )]
   }
 }
